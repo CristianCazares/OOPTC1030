87bb6236bc368818ed7f922ad2bdffa3f5a0408a
Melissa <A01740461@itesm.mx>
2021-05-18 17:20:07 -0600

    [ENHANCEMENT]actividad 3 avance 2

--- Semana2/main.cpp
+++ Semana2/main.cpp
@@ -7,14 +7,31 @@ using namespace std;
 int main(){
     clear();
     Estudiante e1;
+    cout<<"ESTUDIANTES\n";
+    cout<<"(Por constructor default)\n";
     e1.show();
     e1.setNombre("Melissa");
     e1.setEdad(18);
     e1.setCarrera("ITD");
     e1.show();
-
+    cout<<"(Por constructor con parametros)\n";
     Estudiante e2("Cristian","ITC",19);
     e2.show();
+
+
+    Maestro m1;
+    cout<<"\n\nMAESTROS\n";
+    cout<<"(Por constructor default)\n";
+    m1.show();
+    m1.setNombre("Dinaky");
+    m1.setEdad(53);
+    m1.setDepto("Matematicas");
+    m1.show();
+    cout<<"(Por constructor con parametros)\n";
+    Maestro m2("Chiquete","Filosofia y letras",52);
+    m2.show();
+
+
 
     system("PAUSE");
     return 0;
